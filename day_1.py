b25bf4ab04395e011885cbd688d8e0b2ea122c78
George Harris <george.s.harris@gmail.com>
2025-12-01 10:52:10 +0000

    Day 1 part 1 completed!

---
+++ day_1.py
@@ -0,0 +1,66 @@
+######### Part 1 #########
+# Set up dial
+class Dial:
+    def __init__(self):
+        self.current = 50
+        self.high = 99
+        self.low = 0
+
+    def move_right(self):
+        self.current += 1
+        if self.current > self.high:
+            self.current = 0
+    
+    def move_left(self):
+        self.current -= 1
+        if self.current < self.low:
+            self.current = 99
+
+# Import puzzle input
+def import_puzzle_input(filepath: str):
+    with open(filepath) as f:
+        input = f.readlines()
+        input = [x.strip("\n") for x in input]
+    return input
+
+
+# Process instruction
+def process_instruction(instruction: str):
+    direction = instruction[0]
+    places = int(instruction[1:])
+    return direction, places
+
+
+# Move the dial
+def move_dial(dial: Dial, direction: str, places: int):
+    if direction == "L":
+        for _ in range(places):
+            dial.move_left()
+    
+    else:
+        for _ in range(places):
+            dial.move_right()
+
+
+def part_1_script():
+    puzzle_input = import_puzzle_input("puzzle_inputs/day_1.txt")
+    # puzzle_input = [
+    #     "L68", "L30", "R48", 
+    #     "L5", "R60", "L55", 
+    #     "L1", "L99", "R14", 
+    #     "L82"
+    #     ]
+    dial = Dial()
+    times_at_zero = 0
+
+    for instruction in puzzle_input:
+        direction, places = process_instruction(instruction)
+        move_dial(dial, direction, places)
+
+        if dial.current == 0:
+            times_at_zero += 1
+        
+    print(times_at_zero)
+
+part_1_script()
+
